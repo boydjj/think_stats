517f3e01174c2aeb47681969b5395f93969b89cb
Jeremy Boyd <jeremy@jeremy-boyd.com>
2012-12-09 22:52:49 -0600

    Correct misunderstanding of conditional probability; some code cleanup

--- exercises/2_7.py
+++ exercises/2_7.py
@@ -12,29 +12,39 @@ import sys
 import Pmf
 import survey
 
+from first import partition_births
 import utils
+
+def _print_survival_analysis(week, table, label):
+    live_pmf = Pmf.MakePmfFromList([prg.prglength for prg in table.records])
+    new_pmf = utils.remaining_lifetime(live_pmf, week)
+    print 'Probability that a live birth will be in Week {week_num} if not born '\
+        'prior to Week {week_num} ({label}):'.format(week_num=week, label=label),\
+        new_pmf.Prob(week)
+
 
 # On first blush, this is really just another survival analysis problem. So
 # we build a Pmf.Pmf for babies' births and use the same kind of analysis
-# as we did for Exercise 2-4.
+# as we did for Exercise 2-4 using utils.remaining_lifetime.
 if __name__ == '__main__':
     data_dir = sys.argv[1]
     table = survey.Pregnancies()
     table.ReadRecords(data_dir)
-    
-    # Part 1
-    live_pmf = Pmf.MakePmfFromList([prg.prglength for prg in table.records])
-    new_pmf = utils.remaining_lifetime(live_pmf, 38)
-    print 'Probability that a live birth will be in Week 39 if not born '\
-        'prior to Week 39:', new_pmf.Prob(38)
-        
-    # Part 2 isn't really a meaningful task if I'm right in my approach of
-    # using the survival analysis approach, since then Part 1 was just a
-    # special use case. Let's demonstrate some usage.
-    new_pmf = utils.remaining_lifetime(live_pmf, 24)
-    print 'Probability that a live birth will be in Week 25 if not born '\
-        'prior to Week 39:', new_pmf.Prob(24)
+    firsts, others = partition_births(table)
 
-    new_pmf = utils.remaining_lifetime(live_pmf, 49)
-    print 'Probability that a live birth will be in Week 50 if not born '\
-        'prior to Week 39:', new_pmf.Prob(49)
+    # Part 1 - conditional probability that a baby will be born in week 39
+    # Relevant output from official code:
+    # 39 0.633693045564 first babies
+    # 39 0.715792395226 others
+    _print_survival_analysis(39, firsts, 'firsts')
+    _print_survival_analysis(39, others, 'others')
+
+    # Part 2 isn't really a meaningful task if we're right to just use
+    # the survival analysis approach. In that case, Part 1 was just a
+    # special use case. Let's demonstrate this.
+
+    # Relevant output from official code:
+    # 41 0.524781341108 first babies
+    # 41 0.511261261261 others
+    _print_survival_analysis(41, firsts, 'firsts')
+    _print_survival_analysis(41, others, 'others')
